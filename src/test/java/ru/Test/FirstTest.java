ec8a8ab2848dc5ba537d9349f26fc5ce2953ed95
tynu4ok <tynu4ok@mail.ru>
2019-11-20 01:09:36 +0300

    second commit

--- src/test/java/ru/Test/FirstTest.java
+++ src/test/java/ru/Test/FirstTest.java
@@ -4,12 +4,16 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class FirstTest extends WebDriverSettings {
   @Test
   public void firstTest() {
-    driver.get("https://www.yandex.ru/");
+    driver.get("http://localhost/litecart/admin/");
+    driver.findElement(By.name("username")).sendKeys("admin");
+    driver.findElement(By.name("password")).sendKeys("admin");
+    driver.findElement(By.name("login")).click();
+    wait.until(titleIs("My Store"));
   }
 }
 
--- src/test/java/ru/Test/WebDriverSettings.java
+++ src/test/java/ru/Test/WebDriverSettings.java
@@ -6,16 +6,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class WebDriverSettings {
   public ChromeDriver driver;
+  public WebDriverWait wait;
 
   @Before
   public void setUp() {
+    // Тут потом можно в переменную завести проперти и в какой-нить конф. файл, чтобы там был указан PATH драйвера, удобно когда можно передать jar файл + сам драйвер.
     System.setProperty("webdriver.chorome.driver", "/Users/tynu4ok/Downloads/chromedriver_win32/");
     driver = new ChromeDriver();
+    wait = new WebDriverWait(driver,20);
     driver.manage().window().maximize();
   }
 
