e43516fddecc03aae9b58023713e12edc9002d33
tynu4ok <tynu4ok@mail.ru>
2019-12-02 00:20:31 +0300

    v4

--- src/test/java/ru/Test/SecondTest.java
+++ src/test/java/ru/Test/SecondTest.java
@@ -10,18 +10,17 @@ import java.util.List;
 public class SecondTest extends WebDriverSettings {
     @Test
     public void secondTest() {
-
         driver.get("http://localhost/litecart/en/");
 
         List<WebElement> list = driver.findElements(By.cssSelector("li.product"));
 
         for (int i = 0; i < list.size(); i++) {
             WebElement item = list.get(i);
-            System.out.println("Проверяем элемент № " + i);
-           // if (!(item.findElements(By.cssSelector("div[class='sticker sale']")).size() > 0 || item.findElements(By.cssSelector("div[class='sticker new']")).size() > 0)) {
-            if (!(item.findElements(By.cssSelector("div[class='sticker']")).size() > 0 )) {
+            System.out.println("Проверяем элемент № " + (i + 1));
+            // Если более одного класса sticker, выбрасываем ошибку
+            if (item.findElements(By.cssSelector("div.sticker")).size() >= 2 ) {
                 AssertionError assertError = new AssertionError();
-                System.out.println("Ошибка, стикеров более одного или вообще его нет, номер элемента - " + i + " ." + assertError.getMessage());
+                System.out.println("Ошибка, стикеров более одного или вообще его нет, номер элемента - " + (i + 1) + " ." + assertError.getMessage());
                 Assert.fail();
             }
         }
